ae9486a2b35f0b6c9d4e3abfc6a2c9a31d156b56
max00217 <makpung00217@gmail.com>
2023-07-11 20:56:05 +0900

    일단 커밋

--- dbpurge.py
+++ dbpurge.py
@@ -1,17 +1,21 @@
 from models import db, reserv, JJH
 from app import app
 
-@app.before_first_request
 def purgeDB():
     db.create_all()
     reserv_data = reserv.query.all()
     jjh_data = JJH.query.all()
-        
+
     for data in reserv_data:
         db.session.delete(data)
-       
+
     for data in jjh_data:
         db.session.delete(data)
-        
+
     db.session.commit()
+
     print("DB purge complete")
+
+if __name__ == "__main__":
+    with app.app_context():
+        purgeDB()
